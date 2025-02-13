1253c105943deff1754d67bb15b5a33582e6f57f
azamat <azamat.ezhaev@caldera.com>
2025-02-13 11:19:08 +0100

    theme switch

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Manrope } from "next/font/google";
 import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -24,7 +25,16 @@ export default function RootLayout({
 			}}
 		>
 			<html lang="en">
-				<body className={`${manrope.className}`}>{children}</body>
+				<body className={`${manrope.className}`}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
