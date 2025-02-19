0dd14c5bf709016315087ea8e453c9d5c729aab7
Chemberlein <sidorets.kirill97@gmail.com>
2025-02-19 23:33:25 +0100

    Clean up table

--- frontend/app/tables/tokens.tsx
+++ frontend/app/tables/tokens.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { MarketTokensApiService } from "@/services/MarketTokensApiService";
 import { useApi } from "@/hooks/useApi";
 import { TokenOHLCV, TopTokenHolder } from "@/interfaces/tokens";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
-const TopTokenHolders = () => {
+const OHLCTable = () => {
 	const apiService = new MarketTokensApiService();
 	const { data, loading, error } = useApi<TokenOHLCV[]>(() =>
 		apiService.getTokenPriceOHLCV(
 			"5d16cc1a177b5d9ba9cfa9793b07e60f1fb70fea1f8aef064415d114494147",
-			"0be55d262b29f564998ff81efe21bdc0022621c12f15af08d0f2ddb1.39b9b709ac8605fc82116a2efc308181ba297c11950f0f350001e28f0e50868b",
+			"",
 			"1d",
 			365
 		)
@@ -21,16 +22,55 @@ const TopTokenHolders = () => {
 	if (!data) return <div>No data available</div>;
 
 	return (
-		<div>
-			<h1>Top Token Holders</h1>
-			<ul>
-						<li>Time, Open, High, Low, Close</li>
-				{data.map((instance) => (
-						<li>{instance.time}, {instance.open}, {instance.high}, {instance.low}, {instance.close}</li>
-				))}
-			</ul>
+		<div className="flex w-screen">
+			<div className="rounded-md border m-auto" >
+				<Table>
+					<TableHeader>
+						<TableRow>	
+							<TableHead> 
+							Time 
+							</TableHead>
+							<TableHead> 
+							Open
+							</TableHead>
+							<TableHead> 
+							High 
+							</TableHead>
+							<TableHead> 
+							Low
+							</TableHead>
+							<TableHead> 
+							Close
+							</TableHead>	
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+					{
+						data.map((instance) => (
+						<TableRow>
+							<TableCell>	
+								{instance.time}
+							</TableCell>
+							<TableCell>	
+								{instance.open}
+							</TableCell>
+							<TableCell>	
+								{instance.high}
+							</TableCell>
+							<TableCell>	
+								{instance.low}
+							</TableCell>
+							<TableCell>	
+								{instance.open}
+							</TableCell>
+						</TableRow>
+						))
+					}
+					</TableBody>
+				</Table>
+			</div>
 		</div>
 	);
 };
 
-export default TopTokenHolders;
+export default OHLCTable;
