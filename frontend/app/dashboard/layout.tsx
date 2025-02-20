3831b9d15b28c9cc64a5d99ac1a7762e30cbf0bd
azamat <azamat.ezhaev@caldera.com>
2025-02-20 01:18:09 +0100

    starting sidebar rework

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -3,7 +3,14 @@ import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<SidebarProvider>
+		<SidebarProvider
+			style={
+				{
+					"--sidebar-width": "20rem",
+					"--sidebar-width-mobile": "15rem",
+				} as React.CSSProperties
+			}
+		>
 			<AppSidebar />
 			<main>
 				<SidebarTrigger />
--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -1,99 +1,107 @@
+import { Search, Settings, Loader, Radar, Table, Orbit } from "lucide-react";
 import {
-  Calendar,
-  Home,
-  Inbox,
-  Search,
-  Settings,
-  Loader,
-  ChartLine,
-  Radar,
-  Table
-} from 'lucide-react';
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from '@/components/ui/sidebar';
-import { ClerkLoading, ClerkLoaded, UserButton } from '@clerk/nextjs';
-import { ThemeToggle } from '@/components/theme-toggle';
-import { Button } from './ui/button';
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarRail,
+	SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 // Menu items.
 const items = [
-  {
-    title: 'Dashboard',
-    url: '/dashboard',
-    icon: ChartLine,
-  },
-  {
-    title: 'Radar',
-    url: '/scan',
-    icon: Radar,
-  },
-  {
-    title: 'Tables',
-    url: '/tables',
-    icon: Table,
-  },
-  {
-    title: 'Search',
-    url: '#',
-    icon: Search,
-  },
-  {
-    title: 'Settings',
-    url: '#',
-    icon: Settings,
-  },
+	{
+		title: "Solar System",
+		url: "/dashboard",
+		icon: Orbit,
+	},
+	{
+		title: "Radar",
+		url: "/scan",
+		icon: Radar,
+	},
+	{
+		title: "Tables",
+		url: "/tables",
+		icon: Table,
+	},
+	{
+		title: "Search",
+		url: "#",
+		icon: Search,
+	},
+	{
+		title: "Settings",
+		url: "#",
+		icon: Settings,
+	},
 ];
 
 export function AppSidebar() {
-  return (
-    <Sidebar collapsible="icon">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Deep 3D</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <ThemeToggle />
-        <Button
-          variant="outline"
-          className="absolute bottom-0 right-0 mr-4 mb-4 rounded-md p-0 w-10 h-10 flex items-center justify-center"
-        >
-          <ClerkLoading>
-            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-          </ClerkLoading>
-          <ClerkLoaded>
-            <UserButton
-              appearance={{
-                elements: {
-                  userButtonPopoverCard: {
-                    pointerEvents: 'initial',
-                  },
-                },
-              }}
-            />
-          </ClerkLoaded>
-        </Button>
-      </SidebarContent>
-    </Sidebar>
-  );
+	return (
+		<Sidebar collapsible="icon">
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>
+						<div className="flex items-center space-x-2 mb-2">
+							<Image
+								src="/adak.png"
+								alt="logo"
+								width={19}
+								height={19}
+							/>
+							<p>ADA Kosmos</p>
+						</div>
+					</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton
+										asChild
+										className="[&>svg]:size-5"
+									>
+										<a href={item.url}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				<SidebarSeparator />
+				<SidebarRail />
+				<ThemeToggle />
+				<Button
+					variant="outline"
+					className="absolute bottom-0 right-0 mr-4 mb-4 rounded-md p-0 w-10 h-10 flex items-center justify-center"
+				>
+					<ClerkLoading>
+						<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<UserButton
+							appearance={{
+								elements: {
+									userButtonPopoverCard: {
+										pointerEvents: "initial",
+									},
+								},
+							}}
+						/>
+					</ClerkLoaded>
+				</Button>
+			</SidebarContent>
+		</Sidebar>
+	);
 }
